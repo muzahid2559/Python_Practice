62e58310953767c2c4c373c1018dc1dcb5af24aa
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-17 00:42:11 +0600

    variable

--- FirstCode.py
+++ FirstCode.py
@@ -1,10 +1,12 @@
-# Operator (+ , - , * , / , % ,**)
+# Variables
 
-print(4+4)
-print(8-3)
-print(8*3)
-print(8/3)
-print(10%3)
-print(2**3)
-print(10-9*5)
+name = "Muzahid"
+age = 23
+cgpa = 4.60
+
+print("our new student name is "+name)
+print(name+" Live in Dhaka")
+print("He is currently", age , "years old")
+print("At the age of",age ,"he has started to learn Python")
+print(name+"has scored",cgpa,"in her Secondary exam")
 
