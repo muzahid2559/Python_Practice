274474e5e497c89d198bdb619230a122457737ba
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-18 01:58:10 +0600

    A few programs with loops(series)

--- FirstCode.py
+++ FirstCode.py
@@ -1,18 +1,37 @@
-# use of infinite Loop and break statement
-while True:
-    name = input("Please Enter your Name :")
-    if name == "Muzahid":
-        break
-    print(name)
-print("Thank You")
+# series - 1+2+3+4
+sum = 0
+for i in range(1,5):
+    sum = sum +i
+    print(i)
+print("Sum =", sum)
 
-# use of infinite Loop and continue statement
-while True:
-    name = input("Who are you :")
-    if name == "Batman":
-        continue
-    print("Hello There "+name+" What is your password")
-    password = input()
-    if password == "icecream":
-        break
-print("Thank You")
+
+# series - 1^2+2^2+3^2+4^2
+sum = 0
+for i in range(1,5):
+    sum = sum +i*i
+    print(i*i)
+print("Sum =", sum)
+
+
+# series - 1+3+5+7
+odd = 0
+for i in range(1,8,2):
+    odd = odd +i
+    print(i)
+print("Sum =", odd)
+
+
+# series - 2+4+6+8
+even = 0
+for i in range(0,9,2):
+    even = even +i
+    print(i)
+print("Sum =", even)
+
+# complex series - 1+(1+2)+(1+2+3)+(1+2+3+4)
+sum = 0
+for i in range(1,5):
+    for j in range(1,i+1):
+        sum = sum+j
+print(sum)
