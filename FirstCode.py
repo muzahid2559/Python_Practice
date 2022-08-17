f9fb06669fbef4b0dcd8be93ca5c1380e1d82aa8
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-17 23:17:20 +0600

    Conditional Statement

--- FirstCode.py
+++ FirstCode.py
@@ -1,13 +1,10 @@
-# Swapping
-
-print("Enter two variable: ")
-a = int (input())
-b = int (input())
-temp = 0
-(temp) = a
-a = b
-b = temp
-
-print("After Swapping : ")
-print("a = ",a)
-print("b = ",b)
+# Conditional Statement
+print("Type front or back or anything")
+print('Enter Your Command : ' , end="")
+robot_move = input()
+if robot_move == "front" :
+    print("Moving Front")
+elif robot_move == "back" :
+    print("Moving Back")
+else:
+    print("Stand Still")
