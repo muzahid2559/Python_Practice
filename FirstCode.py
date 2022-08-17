92b62154f5dd966a13d808ccce9f95cdbba1e201
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-17 19:47:49 +0600

    Swapping

--- FirstCode.py
+++ FirstCode.py
@@ -1,18 +1,13 @@
-# Getting User Input
+# Swapping
 
-# input always string
-name = input("Enter Your Name: ")
-age = input("Enter Your Age: ")
-gpa = input("Enter Your gpa: ")
+print("Enter two variable: ")
+a = int (input())
+b = int (input())
+temp = 0
+(temp) = a
+a = b
+b = temp
 
-print(" Student Information")
-print("----------------------")
-print("Name : "+name)
-print("Age : "+age)
-print("GPA : "+gpa)
-
-num1 = input("Enter first number: ")
-num2 = input("Enter first number: ")
-
-sum = int(num1) + int(num2)
-print("The sum is ", sum)
+print("After Swapping : ")
+print("a = ",a)
+print("b = ",b)
