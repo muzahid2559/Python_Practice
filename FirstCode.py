d8ce89245c017dbff45b0758ebb761ae04a64943
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-18 03:30:04 +0600

    Exception Handling

--- FirstCode.py
+++ FirstCode.py
@@ -1,22 +1,9 @@
-# Global Scope and Local Scope
+# Exception Handling
 
-def f():
-    c = 10
-    print(c)
-
-f()
-
-
-def function():
-    global  a
-    print(a+23)
-
-function()
-
-
-def pop(): # this code output is error
-    print(x)
-    x = 50
-
-x =100
-pop()
+def fun():
+    try :
+        return 100/x
+    except:
+        return "there is a zero division error"
+    ans = fun(0)
+    print(ans)
