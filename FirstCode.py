bff72920d615f86410b674bd99e9058fc90ec5ed
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-18 02:08:02 +0600

    A program to check whether a year is leap year or not

--- FirstCode.py
+++ FirstCode.py
@@ -1,37 +1,11 @@
-# series - 1+2+3+4
-sum = 0
-for i in range(1,5):
-    sum = sum +i
-    print(i)
-print("Sum =", sum)
+# A program to check whether a year is leap year or not
 
-
-# series - 1^2+2^2+3^2+4^2
-sum = 0
-for i in range(1,5):
-    sum = sum +i*i
-    print(i*i)
-print("Sum =", sum)
-
-
-# series - 1+3+5+7
-odd = 0
-for i in range(1,8,2):
-    odd = odd +i
-    print(i)
-print("Sum =", odd)
-
-
-# series - 2+4+6+8
-even = 0
-for i in range(0,9,2):
-    even = even +i
-    print(i)
-print("Sum =", even)
-
-# complex series - 1+(1+2)+(1+2+3)+(1+2+3+4)
-sum = 0
-for i in range(1,5):
-    for j in range(1,i+1):
-        sum = sum+j
-print(sum)
+year = int(input("Enter the year to check :"))
+if year % 400 == 0:
+    print("It is a leap year")
+elif year % 100 == 0:
+    print("It is not a leap year")
+elif year % 4 == 0:
+    print("It is a leap year")
+else :
+    print("It is not a leap year")
