a30f896e330469f8fbb4599b09f14a13a0eb3f38
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-18 00:00:55 +0600

    Find the largest number out of three number(ConditionalStatement)

--- FirstCode.py
+++ FirstCode.py
@@ -1,10 +1,17 @@
 # Conditional Statement
-print("Type front or back or anything")
-print('Enter Your Command : ' , end="")
-robot_move = input()
-if robot_move == "front" :
-    print("Moving Front")
-elif robot_move == "back" :
-    print("Moving Back")
-else:
-    print("Stand Still")
+# Find the largest number out of three numbers
+
+A = input('Enter First Number : ')
+B = input('Enter Second Number : ')
+C = input('Enter First Number : ')
+
+if A > B:
+    if A > C:
+        print(f"{A} is Greater than {B} and {C}")
+    else:
+        print(f"{C} is Greater than {A} and {B}")
+if A < B:
+    if C < B:
+        print(f"{B} is Greater than {A} and {C}")
+    else:
+        print(f"{C} is Greater than {A} and {B}")
