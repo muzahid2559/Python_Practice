efbb3b31c5b1374aa1a18bb2fad4000e18cb1dad
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-18 00:24:56 +0600

    Ternary Operator

--- FirstCode.py
+++ FirstCode.py
@@ -1,17 +1,9 @@
-# Conditional Statement
-# Find the largest number out of three numbers
+# Ternary Operator
+# Find the largest number out of two numbers
 
 A = input('Enter First Number : ')
 B = input('Enter Second Number : ')
-C = input('Enter Third Number : ')
 
-if A > B:
-    if A > C:
-        print(f"{A} is Greater than {B} and {C}")
-    else:
-        print(f"{C} is Greater than {A} and {B}")
-if A < B:
-    if C < B:
-        print(f"{B} is Greater than {A} and {C}")
-    else:
-        print(f"{C} is Greater than {A} and {B}")
+Max = A if A > B else B  # Ternary Operator
+
+print("largest number ", Max)
