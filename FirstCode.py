fda005c9aa062d263530195ab936dba1d7f107fd
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-19 15:51:34 +0600

    Linear Search

--- FirstCode.py
+++ FirstCode.py
@@ -1,5 +1,13 @@
-# Assuming multiple values at once:
+# Linear Search:
 
-chocolate_milk = ['chocolate','milk','ice_cream']
-x,y,z = chocolate_milk
-print(x,y,z)
+def linear_search(some_list,value):
+    for i in range(len(some_list)):
+        if some_list[i] == value:
+            return "Found"
+    if i == len(some_list)-1:
+        return "Not Found"
+
+
+my = [1,5,8,7]
+b = linear_search(my,8)
+print(b)
