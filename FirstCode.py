2918f9ae21e0fa9d0097f1d9261304f2e44836a1
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-20 00:43:29 +0600

    Methods of Dictionaries

--- FirstCode.py
+++ FirstCode.py
@@ -1,11 +1,33 @@
-# concatinating two Dictionaries
-
-x = {1:8, 2:9}
-y = {3:9, 4:8}
-
-z = x.copy()
-z.update(y)
-print(z)
+# Methods of Dictionaries
+identity = {'Name':'Muzahid','Age':'22','Job':'Student'}
 
 
+# printing value
+print(identity.values())
 
+# printing keys
+for i in identity.keys():
+    print(i)
+
+# printing value and keys
+for i in identity.items():
+    print(i)
+
+# A handy trick
+for k,v in identity.items():
+    print('Key:'+ k +' value:'+v)
+
+# use of 'in' keyword
+print('Name' in identity)
+print('pop' in identity)
+
+# the get() method
+print(str(identity.get('Name','DEFS')))
+print(str(identity.get('pop','DEFS')))
+
+# setdefault() Method
+
+print(identity.setdefault('Name','DEFS'))
+print(identity.setdefault('Height','12fit'))
+
+print(identity)
