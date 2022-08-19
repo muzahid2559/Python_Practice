e011dd5dfffbdc9bc7186981a6227f17d0d1c6de
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-19 19:48:36 +0600

    File

--- FirstCode.py
+++ FirstCode.py
@@ -1,26 +1,21 @@
-# Binary Search:
-
-def binary_search(some_list,value):
-
-        if value>some_list[-1]:
-            return "Not Found"
-        first = 0
-        last = len(some_list)-1
-
-        while first <= last:
-            mid =(first+last)//2 # integer division
-
-            if some_list[mid] == value:
-                return  "Found"
-            elif some_list[mid]>value:
-                first = mid+1
-            elif some_list[mid] < value:
-                last = mid - 1
-
-        if first>last:
-            return "Not Found"
+# Open a file
+f = open("a.txt","w")
 
 
+# getting some info
 
-a = [1,5,7,8,9]
-print(binary_search(a,7))
+print("name =",f.name)
+print("is it closed? ",f.closed)
+print("mode",f.mode)
+f.write("Python is my favorite language.")
+
+# appending to a file
+
+f = open("a.txt","a")
+f.write("I also love JavaScript.")
+f.close()
+
+# r+ functionaly
+f = open("a.txt","r+")
+info = f.read()
+print(info)
