4950f6adc59e251894f64656cf291ae6d9b55743
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-19 16:06:47 +0600

    Binary Search

--- FirstCode.py
+++ FirstCode.py
@@ -1,13 +1,26 @@
-# Linear Search:
+# Binary Search:
 
-def linear_search(some_list,value):
-    for i in range(len(some_list)):
-        if some_list[i] == value:
-            return "Found"
-    if i == len(some_list)-1:
-        return "Not Found"
+def binary_search(some_list,value):
+
+        if value>some_list[-1]:
+            return "Not Found"
+        first = 0
+        last = len(some_list)-1
+
+        while first <= last:
+            mid =(first+last)//2 # integer division
+
+            if some_list[mid] == value:
+                return  "Found"
+            elif some_list[mid]>value:
+                first = mid+1
+            elif some_list[mid] < value:
+                last = mid - 1
+
+        if first>last:
+            return "Not Found"
 
 
-my = [1,5,8,7]
-b = linear_search(my,8)
-print(b)
+
+a = [1,5,7,8,9]
+print(binary_search(a,7))
