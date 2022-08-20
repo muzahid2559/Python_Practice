eca94bd8cb9e5d4f30249be5037fe3b6418504d9
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-20 15:34:58 +0600

    frequency of character on a string( Dictionaries)

--- FirstCode.py
+++ FirstCode.py
@@ -1,33 +1,10 @@
-# Methods of Dictionaries
-identity = {'Name':'Muzahid','Age':'22','Job':'Student'}
+# frequency of character on a string( Dictionaries)
+import pprint as pp
+text = 'this is a simple text to TEST the code.'
+letters = {}
+for i in text:
+    letters.setdefault(i,0)
+    letters[i] = letters[i]+1
 
 
-# printing value
-print(identity.values())
-
-# printing keys
-for i in identity.keys():
-    print(i)
-
-# printing value and keys
-for i in identity.items():
-    print(i)
-
-# A handy trick
-for k,v in identity.items():
-    print('Key:'+ k +' value:'+v)
-
-# use of 'in' keyword
-print('Name' in identity)
-print('pop' in identity)
-
-# the get() method
-print(str(identity.get('Name','DEFS')))
-print(str(identity.get('pop','DEFS')))
-
-# setdefault() Method
-
-print(identity.setdefault('Name','DEFS'))
-print(identity.setdefault('Height','12fit'))
-
-print(identity)
+pp.pprint(letters)
