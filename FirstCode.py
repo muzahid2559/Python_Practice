7ead5b72b0c15d61fa53cda7f8c8aabc105f52b6
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-20 16:18:27 +0600

    Simulations a password protected app access( Dictionaries)

--- FirstCode.py
+++ FirstCode.py
@@ -1,10 +1,25 @@
-# frequency of character on a string( Dictionaries)
-import pprint as pp
-text = 'this is a simple text to TEST the code.'
-letters = {}
-for i in text:
-    letters.setdefault(i,0)
-    letters[i] = letters[i]+1
+# Simulations a password protected app access( Dictionaries)
 
+password_bank = {'Sam':1234,'Smith':909090,'Ruth':120987}
+matched = False
+x = 0 # loop control variable
 
-pp.pprint(letters)
+print("Enter your name: ")
+while x < 5:
+    name = input()
+    if name in password_bank:
+        for i in range(0,3):
+            print("Enter your password :")
+            password = input()
+            if int(password) in password_bank.values():
+                matched = True
+                print('Access Granted.')
+                break
+            else:
+                print('Wrong Password. Enter again: '+'You have '+str(2-i)+' tries left')
+    else:
+        print("There is no such name in the password_bank.Try again")
+    x = x+1
+
+    if matched:
+        break
